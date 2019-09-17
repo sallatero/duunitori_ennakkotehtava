b4ccda9412625ca0a2836548e76243a6ecf1a4e8
Salla <salla.tero@gmail.com>
2019-09-17 17:59:23 +0300

    Moved index.js to the root

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './App'
-//import 'max-700px.css'
-//import 'min-700px.css'
+import App from './src/App'
 
 ReactDOM.render(<App />, document.getElementById('root'));
